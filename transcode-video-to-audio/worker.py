7a24f82a9405376190097cfbb44ed535d99e47dc
Sean Maday <smaday@smaday-macbookpro.roam.corp.google.com>
2019-02-13 11:19:09 -0700

    Added license verbiage to transcode-video-to-audio files

--- transcode-video-to-audio/worker.py
+++ transcode-video-to-audio/worker.py
@@ -1,16 +1,22 @@
-"""Copyright 2018 Google LLC
+#!/usr/bin/env python
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# This is not an officially supported Google product, though support
+# will be provided on a best-effort basis.
 
-    https://www.apache.org/licenses/LICENSE-2.0
-    
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License."""
+# Copyright 2018 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.
+
+# You may obtain a copy of the License at:
+
+#    https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 import os
@@ -31,8 +37,8 @@ dirPath = os.path.normpath(os.getcwd())
 credentialsJson = "__Credential_JSON_File_Name__"
 
 projectId = "__GCP_Project_ID__"
-topicName = "__Pubsub_Topic_Name__"
-subName = "__Pubsub_Subscription_Name__"
+topicName = "transcodeQueue"
+subName = "media-transcode-subscription"
 
 bucketName = "__GCS_Storage_Bucket_Name__"
 utility_service_url = "__Utility_Service_URL__"
